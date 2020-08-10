bc34dff77824597b4488c52247d95b7ea80ef95e
hun-a <seunghun.chan@gmail.com>
2020-08-10 23:08:08 +0900

    Add the Game Scene

---
+++ src/scenes/Game.ts
@@ -0,0 +1,16 @@
+import * as Phaser from 'phaser';
+
+export default class Game extends Phaser.Scene {
+
+  constructor() {
+    super('game');
+  }
+
+  preload() {
+    this.load.image('background', 'house/bg_repeat_340x640.png');
+  }
+
+  create() {
+    this.add.image(0, 0, 'background').setOrigin(0);
+  }
+}
