35ab7416444e9e155136deefcbe0d6329d1f11aa
hun-a <seunghun.chan@gmail.com>
2020-08-12 01:39:24 +0900

    Run with velocity

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -26,7 +26,11 @@ export default class Game extends Phaser.Scene {
 
     const body = mouse.body as Phaser.Physics.Arcade.Body;
     body.setCollideWorldBounds(true);
+    body.setVelocityX(200);
 
     this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30);
+
+    this.cameras.main.startFollow(mouse);
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height);
   }
 }
