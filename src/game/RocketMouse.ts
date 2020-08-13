1b8392329ad6ff7b033b39f1e771a2d7e2e173a2
hun-a <seunghun.chan@gmail.com>
2020-08-14 01:14:15 +0900

    Creating a RocketMouse class

---
+++ src/game/RocketMouse.ts
@@ -0,0 +1,16 @@
+import Phaser from 'phaser';
+import TextureKeys from "~/consts/TextureKeys";
+import AnimationKeys from "~/consts/AnimationKeys";
+
+export default class RocketMouse extends Phaser.GameObjects.Container {
+
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    super(scene, x, y);
+
+    const mouse = scene.add.sprite(0, 0, TextureKeys.RocketMouse)
+      .setOrigin(0.5, 1)
+      .play(AnimationKeys.RocketMouseRun);
+
+    this.add(mouse);
+  }
+}
