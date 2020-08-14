f18c6a806716c96acbee8ebe0fc7cfbe74935593
hun-a <seunghun.chan@gmail.com>
2020-08-14 22:19:12 +0900

    Adding Thrust

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -7,6 +7,8 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
 
   private flames: Phaser.GameObjects.Sprite;
 
+  private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
 
@@ -27,9 +29,27 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
     const body = this.body as Phaser.Physics.Arcade.Body;
     body.setSize(mouse.width, mouse.height);
     body.setOffset(mouse.width * -0.5, -mouse.height);
+
+    this.cursors = scene.input.keyboard.createCursorKeys();
   }
 
   enableJetpack(enabled: boolean) {
     this.flames.setVisible(enabled);
+  }
+
+  preUpdate() {
+    const body = this.body as Phaser.Physics.Arcade.Body;
+
+    // Check is Space bar is down
+    if (this.cursors.space?.isDown) {
+      // Set y acceleration to -600 if so
+      body.setAccelerationY(-600);
+      this.enableJetpack(true);
+    } else {
+      // Turn off acceleration otherwise
+      body.setAccelerationY(0);
+      this.enableJetpack(false);
+
+    }
   }
 }
